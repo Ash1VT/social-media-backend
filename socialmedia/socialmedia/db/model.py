b8cf688b018dae76c6429b4c0a77975bee1b569d
Ash1VT <nikita.panasevitch@yandex.by>
2022-11-07 21:15:11 +0300

    expanded user model

--- socialmedia/socialmedia/db/model.py
+++ socialmedia/socialmedia/db/model.py
@@ -1,9 +1,12 @@
+from sqlalchemy.orm import validates
 from setup.database import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(), nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
     refresh_token_jti = db.Column(db.String(), nullable=True)
