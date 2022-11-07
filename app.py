a300a41dba846d7913638f5437d4a9bde368dcbe
Ash1VT <nikita.panasevitch@yandex.by>
2022-11-07 21:31:05 +0300

    updated blueprints configuration, fix cors problem

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from setup import app, db, jwt
-from config import JWT_ACCESS_TOKEN_EXPIRES, JWT_REFRESH_TOKEN_EXPIRES, \
-    JWT_SECRET_KEY, JWT_TOKEN_LOCATION, JWT_CSRF_CHECK_FORM, JWT_CSRF_IN_COOKIES,\
-    JWT_COOKIE_CSRF_PROTECT, is_token_valid
-from socialmedia.socialmedia.api import blueprint_token
+from flask import Blueprint
+from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
 
+from setup import app, db, jwt
+from config import is_token_valid, token_expired_response, missing
+from socialmedia.socialmedia.api.blueprints import login_blueprint, \
+    token_blueprint, user_blueprint
+
+CORS(app)
 app.config.from_object('config.app.DevelopmentConfig')
+app.config.from_object('config.jwt.JWTConfig')
 db.init_app(app)
 
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = JWT_REFRESH_TOKEN_EXPIRES
-app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
-app.config['JWT_CSRF_CHECK_FORM'] = JWT_CSRF_CHECK_FORM
-app.config['JWT_CSRF_IN_COOKIES'] = JWT_CSRF_IN_COOKIES
-app.config['JWT_COOKIE_CSRF_PROTECT'] = JWT_COOKIE_CSRF_PROTECT
 
 jwt.token_verification_loader(is_token_valid)
+jwt.expired_token_loader(token_expired_response)
+jwt.unauthorized_loader(missing)
 
+api_blueprint = Blueprint('api_blueprint', import_name=__name__)
+api_blueprint.register_blueprint(login_blueprint, url_prefix='/login')
+api_blueprint.register_blueprint(user_blueprint, url_prefix='/users')
+api_blueprint.register_blueprint(token_blueprint, url_prefix='/token')
 
-app.register_blueprint(blueprint_token, url_prefix='/api/')
+app.register_blueprint(api_blueprint, url_prefix='/api/')
+
