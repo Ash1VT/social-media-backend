522ad133efd65fd5e621487773ef17ea7ea5e356
Ash1VT <nikita.panasevitch@yandex.by>
2022-11-13 00:54:56 +0300

    added jwt config initialization

--- setup/jwt.py
+++ setup/jwt.py
@@ -1,5 +1,14 @@
 from flask_jwt_extended import JWTManager
 
+from socialmedia.responses import get_token_expired_response, get_token_invalid_response, \
+    get_token_missing_response, get_token_revoked_response
+from socialmedia.api.jwt import check_if_token_is_revoked
 from . import app
 
 jwt = JWTManager(app)
+
+jwt.token_in_blocklist_loader(check_if_token_is_revoked)
+jwt.expired_token_loader(get_token_expired_response)
+jwt.revoked_token_loader(get_token_revoked_response)
+jwt.invalid_token_loader(get_token_invalid_response)
+jwt.unauthorized_loader(get_token_missing_response)
